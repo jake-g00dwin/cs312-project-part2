957b40331aded55c73a6bca0f471fc1596bb4cc8
jakeg00dwin <jakegoodwin@gorge.works>
2024-06-09 13:36:41 -0700

    Added a script to automate the whole process

---
+++ runit.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Author: Jake G
+# Date: 2024
+# Filename: runit.sh
+
+
+function test_terraform () {
+    echo "Now tesing Terrform..."
+    sleep 2
+    cd ./src
+    terraform init
+    terraform fmt
+    terraform apply
+    cd ../
+}
+
+test_terraform
