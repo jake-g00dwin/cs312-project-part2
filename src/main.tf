a974eba8cfe3e0639cc7a16673c061d702ab963e
jakeg00dwin <jakegoodwin@gorge.works>
2024-06-09 13:48:06 -0700

    ignore the backup

--- src/main.tf
+++ src/main.tf
@@ -17,49 +17,49 @@ terraform {
 
 // This setup for the provider assumes you have a credentials file.
 provider "aws" {
-  profile = "default"
-  region  = "us-east-1"
+    profile = "default"
+    region  = "us-east-1"
 }
 
 
 // Setup for the VPC
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-
-  tags = {
-    Name = "minecraft-vpc"
-  }
+    cidr_block = "10.0.0.0/16"
+    
+    tags = {
+        Name = "minecraft-vpc"
+    }
 }
 
 
 // Setup the subnet
 resource "aws_subnet" "main" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.10.10.0/24"
-  availability_zone = "us-east-1"
+    vpc_id = aws_vpc.main.id
+    cidr_block = "10.10.10.0/24"
+    availability_zone = "us-east-1a"
 }
 
 
 // Setup gateway, we need the net up in here
 resource "aws_internet_gateway" "main" {
-  vpc_id = aws_vpc.main.id
+    vpc_id = aws_vpc.main.id
 }
 
 
 // Need a routing table too
 resource "aws_route_table" "main" {
-  vpc_id = aws_vpc.main.id
+    vpc_id = aws_vpc.main.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.main.id
   }
 }
 
 
 resource "aws_route_table_association" "main" {
-  subnet_id      = aws_subnet.main.id
-  route_table_id = aws_route_table.main.id
+    subnet_id = aws_subnet.main.id
+    route_table_id = aws_route_table.main.id
 }
 
 
@@ -67,44 +67,44 @@ resource "aws_route_table_association" "main" {
 resource "aws_security_group" "minecraft-sg" {
   vpc_id = aws_vpc.main.id
 
-  // Allow inbound SSH traffic
-  // I guess I could change the port if I felt fancy meh
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+    // Allow inbound SSH traffic
+    // I guess I could change the port if I felt fancy meh
+    ingress {
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 
-  // Allow the minecraft server connections
-  ingress {
-    from_port   = 25565
-    to_port     = 25565
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+    // Allow the minecraft server connections
+    ingress {
+        from_port   = 25565
+        to_port     = 25565
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 }
 
 
 
 // Create the Instance
 resource "aws_instance" "minecraft" {
-  // Amazon Linux AMI(2023)
-  ami             = "ami-00beae93a2d981137"
-  instance_type   = "t2.micro"
-  security_groups = [aws_security_group.minecraft-sg.name]
-  subnet_id       = aws_subnet.main.id
-  key_name        = "labuser.pem"
+    // Amazon Linux AMI(2023)
+    ami = "ami-00beae93a2d981137"
+    instance_type = "t2.micro"
+    security_groups = [aws_security_group.minecraft-sg.name]
+    subnet_id = aws_subnet.main.id
+    key_name = "labuser.pem"
 
-  tags = {
-    Name = var.instance_name
-  }
+    tags = {
+        Name = var.instance_name
+    }
 }
 
