caaef1e6864472e813757223c67c7b91c450a024
jakeg00dwin <jakegoodwin@gorge.works>
2024-06-09 14:55:01 -0700

    Added line to get public ipv4 address

--- src/main.tf
+++ src/main.tf
@@ -98,11 +98,12 @@ resource "aws_security_group" "main" {
 
 // Create the Instance
 resource "aws_instance" "minecraft" {
-  ami                    = var.ami
-  instance_type          = var.instance_type
-  subnet_id              = aws_subnet.main.id
-  vpc_security_group_ids = [aws_security_group.main.id]
-  key_name               = var.key_name
+  ami                         = var.ami
+  instance_type               = var.instance_type
+  associate_public_ip_address = true
+  subnet_id                   = aws_subnet.main.id
+  vpc_security_group_ids      = [aws_security_group.main.id]
+  key_name                    = var.key_name
 
   tags = {
     Name = var.instance_name
